a8eddb65b69abb3cf5b238661393b072ac506de9
Wojtek Mach <wojtek@wojtekmach.pl>
2018-01-18 23:11:52 +0100

    Hardcode versions

--- run.exs
+++ run.exs
@@ -1,18 +1,20 @@
-app = :elixir
-{:ok, modules} = :application.get_key(app, :modules)
+for app <- [:elixir, :mix, :eex, :logger] do
+  :ok = Application.ensure_started(app)
+  {:ok, modules} = :application.get_key(app, :modules)
 
-for module <- modules do
-  docs = Code.get_docs(module, :all)
+  for module <- modules do
+    docs = Code.get_docs(module, :all)
 
-  case docs[:moduledoc] do
-    {_, moduledoc} when is_binary(moduledoc) ->
-      for {{fun, arity}, _, _kind, _, _doc} <- docs[:docs] do
-        IO.puts "#{inspect module}.#{fun}/#{arity}"
-      end
+    case docs[:moduledoc] do
+      {_, moduledoc} when is_binary(moduledoc) ->
+        for {{fun, arity}, _, _kind, _, _doc} <- docs[:docs] do
+          IO.puts "#{inspect module}.#{fun}/#{arity}"
+        end
 
-      IO.puts ""
+        IO.puts ""
 
-    _ ->
-      :skip
+      _ ->
+        :skip
+    end
   end
 end
--- run.sh
+++ run.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-# Usage: /path/to/run.sh "v1.4 v1.5 v1.6 master'
+# Usage: /path/to/run.sh
+#
+# Run from within git repository.
 
-versions=$1
+versions="v1.0 v1.1 v1.2 v1.3 v1.4 v1.5 v1.6 master"
+pairs="v1.0,v1.1 v1.1,v1.2 v1.3,v1.4 v1.4,v1.5 v1.5,v1.6 v1.6,master"
 run_exs=${0/.sh/.exs}
 
 for v in $versions; do
   git checkout $v
   make clean compile
-  elixir $run_exs > modules-$v.md
+  elixir $run_exs > modules-$v.txt
+done
+
+for pair in $pairs; do
+  IFS=',' read -r -a array <<< "$pair"
+  left=${array[0]}
+  right=${array[1]}
+  git diff --no-index modules-${left}.txt modules-${right}.txt > diff-${left}-${right}.txt.diff
 done
