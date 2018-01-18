45c74ea6bfadbab453f444cc593f7e514f24cb5e
Wojtek Mach <wojtek@wojtekmach.pl>
2018-01-19 00:25:02 +0100

    Only show documented functions

--- run.exs
+++ run.exs
@@ -1,4 +1,5 @@
-for app <- [:elixir, :mix, :eex, :logger] do
+apps = [:elixir, :mix, :eex, :logger, :iex, :ex_unit]
+for app <- apps do
   :ok = Application.ensure_started(app)
   {:ok, modules} = :application.get_key(app, :modules)
 
@@ -7,8 +8,10 @@ for app <- [:elixir, :mix, :eex, :logger] do
 
     case docs[:moduledoc] do
       {_, moduledoc} when is_binary(moduledoc) ->
-        for {{fun, arity}, _, _kind, _, _doc} <- docs[:docs] do
-          IO.puts "#{inspect module}.#{fun}/#{arity}"
+        for {{fun, arity}, _, _kind, _, doc} <- docs[:docs] do
+          if is_binary(doc) do
+            IO.puts "#{inspect module}.#{fun}/#{arity}"
+          end
         end
 
         IO.puts ""
